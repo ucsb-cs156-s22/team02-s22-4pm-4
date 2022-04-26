c0b5b51b396e77db00a3efbc13ab00857287ec4a
Victoria Reed <victoria@169-231-164-65.wireless.ucsb.edu>
2022-04-26 15:14:35 -0700

    vr- working on controller

--- src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/HelpRequestRepository.java
@@ -8,5 +8,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface HelpRequestRepository extends CrudRepository<HelpRequest, Long>{
+    Iterable<HelpRequest> findAll();
     
 }
