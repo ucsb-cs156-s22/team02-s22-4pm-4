5429ad9a656f482367aad4aaf17b455e994aebce
Heather Nguyen <heathernguyen@ResNet-67-191.resnet.ucsb.edu>
2022-04-27 21:39:59 -0700

    hn - finished other functions

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -1,7 +1,9 @@
 package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,6 +17,9 @@ import lombok.Builder;
 @Entity(name = "ucsbdiningcommonsmenuitem")
 public class UCSBDiningCommonsMenuItem {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
   String diningCommonsCode;
   String name;
   String station;
