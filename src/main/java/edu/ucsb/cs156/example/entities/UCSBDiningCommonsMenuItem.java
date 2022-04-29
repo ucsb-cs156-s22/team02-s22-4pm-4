198deddf0416e22f426b630d703d93607fc9a1b8
Wang Haoran(Robin) <notrealrobin@gmail.com>
2022-04-29 12:08:41 -0700

    merge conflicts

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBDiningCommonsMenuItem.java
@@ -1,9 +1,7 @@
 package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,9 +15,6 @@ import lombok.Builder;
 @Entity(name = "ucsbdiningcommonsmenuitem")
 public class UCSBDiningCommonsMenuItem {
   @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private long id;
-
   String diningCommonsCode;
   String name;
   String station;
