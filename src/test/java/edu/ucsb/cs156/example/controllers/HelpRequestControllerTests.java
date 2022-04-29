73abaa45d55c5f19c79e6f1f6f8b3fc65c2a021f
Victoria Reed <victoria@169-231-167-230.wireless.ucsb.edu>
2022-04-29 13:21:32 -0700

    vr - finished controller tests

--- src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
+++ src/test/java/edu/ucsb/cs156/example/controllers/HelpRequestControllerTests.java
@@ -108,7 +108,7 @@ public class HelpRequestControllerTests extends ControllerTestCase{
 
     }
 
-    @WithMockUser(roles = { "USER "})
+    @WithMockUser(roles = { "USER"})
     @Test
     public void test_that_logged_in_user_can_get_by_id_when_the_id_does_not_exist() throws Exception{
 
