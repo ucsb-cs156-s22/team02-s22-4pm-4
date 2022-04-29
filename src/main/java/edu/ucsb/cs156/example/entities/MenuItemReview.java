6f4801866281412fffaa16ab9f55bb25f1056674
Zoey <zoey@Zoeys-MacBook-Pro.local>
2022-04-28 21:54:47 -0700

    finished controller and tests

--- src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
+++ src/main/java/edu/ucsb/cs156/example/entities/MenuItemReview.java
@@ -2,8 +2,9 @@ package edu.ucsb.cs156.example.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-
 import java.time.LocalDateTime;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -15,9 +16,10 @@ import lombok.Builder;
 @NoArgsConstructor
 @Builder
 @Entity(name = "menuitemreview")
-
 public class MenuItemReview {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private Long itemId;
     private String reviewerEmail;
     private int stars;
