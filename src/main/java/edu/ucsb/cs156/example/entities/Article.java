fde26888522d458b8075128430f85c10c7ee9e29
aripo99 <a_polakof@umail.ucsb.edu>
2022-04-30 13:30:46 -0700

    fix naming entity

--- src/main/java/edu/ucsb/cs156/example/entities/Article.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Article.java
@@ -16,7 +16,7 @@ import lombok.Builder;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "article")
+@Entity(name = "Article")
 public class Article {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
