62f6943d5e6be4d4438ccdf820213977edcb96a2
Sebastian Anvari <sebastiananvari@Sebastians-MacBook-Pro-3.local>
2022-05-04 16:36:38 -0700

    updating repository for put and delete

--- src/main/java/edu/ucsb/cs156/example/entities/Recommendation.java
+++ src/main/java/edu/ucsb/cs156/example/entities/Recommendation.java
@@ -3,9 +3,9 @@ package edu.ucsb.cs156.example.entities;
 import java.time.LocalDateTime;
 
 import javax.persistence.Entity;
-//import javax.persistence.GenerationType;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-//import javax.persistence.GeneratedValue;
+import javax.persistence.GeneratedValue;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -19,6 +19,7 @@ import lombok.Builder;
 @Entity(name = "recommendation")
 public class Recommendation {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY) 
   private long id;
   private String requesterEmail;
   private String professorEmail;
